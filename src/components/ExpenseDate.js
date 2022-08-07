9c02f78838cfb8a4585dd0d46f628e9bc0093854
AnDamazio <damazioandre@hotmail.com>
2022-08-07 14:43:30 -0300

    split expense date in a new componente. add capitalize function to turn uppercase the first letter of a word

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,22 @@
+import './ExpenseDate.css';
+
+const capitalize = (text) => {
+    text = text.substr(0, 1).toUpperCase() + text.substr(1);
+    return text;
+}
+
+export default function ExpenseDate(props) {
+  const month = capitalize(props.date.toLocaleString("pt-BR", { month: "long" }));
+  const day = props.date.toLocaleString("pt-BR", { day: "2-digit" });
+  const year = props.date.getFullYear();
+
+capitalize(month)
+
+  return (
+    <div className='expense-date'>
+      <div className='expense-date__month'>{month}</div>
+      <div className='expense-date__year'>{year}</div>
+      <div className='expense-date__day'>{day}</div>
+    </div>
+  );
+}
